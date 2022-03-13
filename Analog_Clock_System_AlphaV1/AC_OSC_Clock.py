34a0e3658d849688b129deb040cab6fc90dc9baa
Kazeniwa-Yui <wind.garden0471@gmail.com>
2022-03-14 06:25:52 +0900

    テスト版,とりあえず動くようにした

--- Analog_Clock_System_AlphaV1/AC_OSC_Clock.py
+++ Analog_Clock_System_AlphaV1/AC_OSC_Clock.py
@@ -32,6 +32,18 @@ client.send_message("/avatar/parameters/AC_hh", float(0.0))
 client.send_message("/avatar/parameters/AC_mh", float(0.0))
 client.send_message("/avatar/parameters/AC_sc", float(0.0))
 
+time.sleep(0.2)
+
+client.send_message("/avatar/parameters/AC_hh", float(0.31))
+client.send_message("/avatar/parameters/AC_mh", float(0.31))
+client.send_message("/avatar/parameters/AC_sc", float(0.31))
+
+time.sleep(0.2)
+
+client.send_message("/avatar/parameters/AC_hh", float(0.6))
+client.send_message("/avatar/parameters/AC_mh", float(0.6))
+client.send_message("/avatar/parameters/AC_sc", float(0.6))
+
 print('\rOSC送信を開始します\n')
 
 print("set_ip: ", ip, "\nset_port: ", port, "\n")
@@ -40,7 +52,7 @@ print('Ctrl+Cで終了してください\n')
 
 #浮動小数点数演算はなんか知らないけどうまくいかないので良い方法思いつくまでゴリ押す
 NUMBER = {
-    00:0.0,
+    00:0.00,
     1:0.01,
     2:0.02,
     3:0.03,
@@ -50,7 +62,7 @@ NUMBER = {
     7:0.07,
     8:0.08,
     9:0.09,
-    10:0.1,
+    10:0.10,
     11:0.11,
     12:0.12,
     13:0.13,
@@ -60,7 +72,7 @@ NUMBER = {
     17:0.17,
     18:0.18,
     19:0.19,
-    20:0.2,
+    20:0.20,
     21:0.21,
     22:0.22,
     23:0.23,
@@ -70,7 +82,7 @@ NUMBER = {
     27:0.27,
     28:0.28,
     29:0.29,
-    30:0.3,
+    30:0.30,
     31:0.31,
     32:0.32,
     33:0.33,
@@ -80,7 +92,7 @@ NUMBER = {
     37:0.37,
     38:0.38,
     39:0.39,
-    40:0.4,
+    40:0.40,
     41:0.41,
     42:0.42,
     43:0.43,
@@ -90,7 +102,7 @@ NUMBER = {
     47:0.47,
     48:0.48,
     49:0.49,
-    50:0.5,
+    50:0.50,
     51:0.51,
     52:0.52,
     53:0.53,
@@ -100,12 +112,42 @@ NUMBER = {
     57:0.57,
     58:0.58,
     59:0.59,
-    60:0.6
+    60:0.60
+}
+
+NUMBER_HOURS = {
+    0:0.0,
+    1:0.01,
+    2:0.02,
+    3:0.03,
+    4:0.04,
+    5:0.05,
+    6:0.06,
+    7:0.07,
+    8:0.08,
+    9:0.09,
+    10:0.10,
+    11:0.11,
+    12:0.12,
+    13:0.01,
+    14:0.02,
+    15:0.03,
+    16:0.04,
+    17:0.05,
+    18:0.06,
+    19:0.07,
+    20:0.08,
+    21:0.09,
+    22:0.10,
+    23:0.11,
+    24:0.12
 }
 
 try:
 
     while True:
+
+        time.sleep(1)
 
         #datetimeモジュールからPCの時刻を取得するよ
         dt_now = datetime.datetime.now()
@@ -115,20 +157,37 @@ try:
         minutes = dt_now.strftime('%M')
         seconds = dt_now.strftime('%S')
 
-        hours_hand = NUMBER[int(hours)]
+        hours_hand = NUMBER_HOURS[int(hours)]
         minutes_hand = NUMBER[int(minutes)]
         seconds_hand = NUMBER[int(seconds)]
 
-        print("\r現在時刻(debug):", hours_hand,":",minutes_hand,":",seconds_hand, end="")
+        print("現在時刻(debug):", hours_hand,":",minutes_hand,":",seconds_hand)
 
         #とんでけーー！！
-        client.send_message("/avatar/parameters/AC_hh", float(hours_hand))
-        client.send_message("/avatar/parameters/AC_mh", float(minutes_hand))
-        client.send_message("/avatar/parameters/AC_sc", float(seconds_hand))
 
-        #飛んでったから一秒待つよ
-        #ここを変更で送信頻度を変更できるよ
-        time.sleep(1)
+        if hours_hand == float(0.12):
+            client.send_message("/avatar/parameters/AC_hh", float(0.11))
+            time.sleep(1)
+            client.send_message("/avatar/parameters/AC_hh", float(0.0))
+
+        else:
+            client.send_message("/avatar/parameters/AC_hh", float(hours_hand))
+
+        if minutes_hand == float(0.59):
+            client.send_message("/avatar/parameters/AC_mh", float(0.59))
+            time.sleep(1)
+            client.send_message("/avatar/parameters/AC_mh", float(0.0))
+
+        else:
+            client.send_message("/avatar/parameters/AC_mh", float(minutes_hand))
+
+        if seconds_hand == float(0.59):
+            client.send_message("/avatar/parameters/AC_sc", float(0.59))
+            time.sleep(1)
+            client.send_message("/avatar/parameters/AC_sc", float(0.0))
+
+        else:
+            client.send_message("/avatar/parameters/AC_sc", float(seconds_hand))
 
 except KeyboardInterrupt:
 
